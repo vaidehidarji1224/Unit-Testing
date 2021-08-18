8c85eacbb9b67ef5320651ec99a2396ab1db2aa5
Vaidehi <Vdarji@confederationcollege.ca>
2021-08-18 12:21:11 -0400

    files added

---
+++ IsItALeapYear/IsItALeapYear/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace IsItALeapYear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public bool IsItALeapYear(int year)
+        {
+            if ((year % 4 == 0) && ( year % 100 != 0))
+            { 
+                return false;
+            }
+            else if (year % 400 == 0)
+            {
+                return true;
+            }
+
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
---
+++ IsItALeapYear/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+       
+           
+            public void Year_2001_Is_not_a_leap_year()
+            {
+                // Arrange
+                var p = new Program();
+                var expected = false;
+                // Act
+                var actual = p.IsItALeapYear(2001);
+                // Assert
+                Assert.AreEqual(expected, actual);
+            }
+
+            //test2
+            [TestMethod]
+            public void Year_1996_Is_a_leap_year()
+            {
+                // Arrange
+                var p = new Program();
+                var expected = true;
+                // Act
+                var actual = p.IsItALeapYear(1996);
+                // Assert
+                Assert.AreEqual(expected, actual);
+            }
+
+            //test3
+            [TestMethod]
+            public void Year_1900_is_not_a_leap_year()
+            {
+                // Arrange
+                var p = new Program();
+                var expected = false;
+                // Act
+                var actual = p.IsItALeapYear(1900);
+                // Assert
+                Assert.AreEqual(expected, actual);
+            }
+
+            //test4
+            [TestMethod]
+            public void Year_2000_is_a_leap_year()
+            {
+                // Arrange
+                var p = new Program();
+                var expected = true;
+                // Act
+                var actual = p.IsItALeapYear(2000);
+                // Assert
+                Assert.AreEqual(expected, actual);
+            }
+        }
+    }
+}
